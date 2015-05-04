ed30efb9f8fe1fdc25fbcaa45f43bcdc620f5828
Jeff Mesnil <jmesnil@gmail.com>
2015-05-04 16:48:29 +0200

    rename ActiveMQ to Artemis

---
+++ src/main/java/org/apache/activemq/artemis/wildfly/integration/recovery/WildFlyActiveMQRecoveryRegistry.java
@@ -0,0 +1,255 @@
+/**
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements. See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package org.apache.activemq.artemis.wildfly.integration.recovery;
+
+import javax.transaction.xa.XAResource;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.activemq.artemis.api.core.Pair;
+import org.apache.activemq.artemis.api.core.TransportConfiguration;
+import org.apache.activemq.artemis.service.extensions.xa.recovery.ActiveMQXAResourceWrapper;
+import org.apache.activemq.artemis.service.extensions.xa.recovery.XARecoveryConfig;
+import org.jboss.tm.XAResourceRecovery;
+
+/**
+* <p>This class is used by the Resource Adapter to register RecoveryDiscovery, which is based on the {@link org.apache.activemq.wildfly.integration.recovery.XARecoveryConfig}</p>
+* <p>Each outbound or inboud connection will pass the configuration here through by calling the method {@link WildFlyActiveMQRecoveryRegistry#register(org.apache.activemq.wildfly.integration.recovery.XARecoveryConfig)}</p>
+* <p>Later the {@link WildFlyRecoveryDiscovery} will call {@link WildFlyActiveMQRecoveryRegistry#nodeUp(String, org.apache.activemq.api.core.Pair, String, String)}
+* so we will keep a track of nodes on the cluster
+* or nodes where this server is connected to. </p>
+*
+* @author clebertsuconic
+*/
+public class WildFlyActiveMQRecoveryRegistry implements XAResourceRecovery
+{
+
+   private static final WildFlyActiveMQRecoveryRegistry theInstance = new WildFlyActiveMQRecoveryRegistry();
+
+   private final ConcurrentHashMap<XARecoveryConfig, WildFlyRecoveryDiscovery> configSet = new ConcurrentHashMap<XARecoveryConfig, WildFlyRecoveryDiscovery>();
+
+   /**
+    * The list by server id and resource adapter wrapper, what will actually be calling recovery.
+    * This will be returned by getXAResources
+    */
+   private final ConcurrentHashMap<String, ActiveMQXAResourceWrapper> recoveries = new ConcurrentHashMap<String, ActiveMQXAResourceWrapper>();
+
+   /**
+    * In case of failures, we retry on the next getXAResources
+    */
+   private final Set<WildFlyRecoveryDiscovery> failedDiscoverySet = new HashSet<WildFlyRecoveryDiscovery>();
+
+   private WildFlyActiveMQRecoveryRegistry()
+   {
+   }
+
+   /**
+    * This will be called periodically by the Transaction Manager
+    */
+   public XAResource[] getXAResources()
+   {
+      try
+      {
+         checkFailures();
+
+         ActiveMQXAResourceWrapper[] resourceArray = new ActiveMQXAResourceWrapper[recoveries.size()];
+         resourceArray = recoveries.values().toArray(resourceArray);
+
+         if (WildFlyActiveMQLogger.LOGGER.isDebugEnabled())
+         {
+            WildFlyActiveMQLogger.LOGGER.debug("\n=======================================================================================");
+            WildFlyActiveMQLogger.LOGGER.debug("Returning the following list on getXAREsources:");
+            for (Map.Entry<String, ActiveMQXAResourceWrapper> entry : recoveries.entrySet())
+            {
+               WildFlyActiveMQLogger.LOGGER.debug("server-id=" + entry.getKey() + ", value=" + entry.getValue());
+            }
+            WildFlyActiveMQLogger.LOGGER.debug("=======================================================================================\n");
+         }
+
+         return resourceArray;
+      }
+      catch (Throwable e)
+      {
+         WildFlyActiveMQLogger.LOGGER.warn(e.getMessage(), e);
+         return new XAResource[]{};
+      }
+   }
+
+   public static WildFlyActiveMQRecoveryRegistry getInstance()
+   {
+      return theInstance;
+   }
+
+   /**
+    * This will be called by then resource adapters, to register a new discovery
+    *
+    * @param resourceConfig
+    */
+   public void register(final XARecoveryConfig resourceConfig)
+   {
+      WildFlyRecoveryDiscovery newInstance = new WildFlyRecoveryDiscovery(resourceConfig);
+      WildFlyRecoveryDiscovery discoveryRecord = configSet.putIfAbsent(resourceConfig, newInstance);
+      if (discoveryRecord == null)
+      {
+         discoveryRecord = newInstance;
+         discoveryRecord.start(false);
+      }
+      // you could have a configuration shared with multiple MDBs or RAs
+      discoveryRecord.incrementUsage();
+   }
+
+   /**
+    * Reference counts and deactivate a configuration
+    * Notice: this won't remove the servers since a server may have previous XIDs
+    *
+    * @param resourceConfig
+    */
+   public void unRegister(final XARecoveryConfig resourceConfig)
+   {
+      WildFlyRecoveryDiscovery discoveryRecord = configSet.get(resourceConfig);
+      if (discoveryRecord != null && discoveryRecord.decrementUsage() == 0)
+      {
+         discoveryRecord = configSet.remove(resourceConfig);
+         if (discoveryRecord != null)
+         {
+            discoveryRecord.stop();
+         }
+      }
+   }
+
+   /**
+    * We need to make sure that all resources are closed, we don't actually do this when a resourceConfig is closed but
+    * maybe we should.
+    */
+   public void stop()
+   {
+      for (WildFlyRecoveryDiscovery recoveryDiscovery : configSet.values())
+      {
+         recoveryDiscovery.stop();
+      }
+      for (ActiveMQXAResourceWrapper activeMQXAResourceWrapper : recoveries.values())
+      {
+         activeMQXAResourceWrapper.close();
+      }
+      recoveries.clear();
+      configSet.clear();
+   }
+
+   /**
+    * in case of a failure the Discovery will register itslef to retry
+    *
+    * @param failedDiscovery
+    */
+   public void failedDiscovery(WildFlyRecoveryDiscovery failedDiscovery)
+   {
+      WildFlyActiveMQLogger.LOGGER.debug("RecoveryDiscovery being set to restart:" + failedDiscovery);
+      synchronized (failedDiscoverySet)
+      {
+         failedDiscoverySet.add(failedDiscovery);
+      }
+   }
+
+   /**
+    * @param nodeID
+    * @param networkConfiguration
+    * @param username
+    * @param password
+    */
+   public void nodeUp(String nodeID,
+                      Pair<TransportConfiguration, TransportConfiguration> networkConfiguration,
+                      String username,
+                      String password)
+   {
+
+      if (recoveries.get(nodeID) == null)
+      {
+         if (WildFlyActiveMQLogger.LOGGER.isDebugEnabled())
+         {
+            WildFlyActiveMQLogger.LOGGER.debug(nodeID + " being registered towards " + networkConfiguration);
+         }
+         XARecoveryConfig config = new XARecoveryConfig(true,
+                                                        extractTransportConfiguration(networkConfiguration),
+                                                        username,
+                                                        password);
+
+         ActiveMQXAResourceWrapper wrapper = new ActiveMQXAResourceWrapper(config);
+         recoveries.putIfAbsent(nodeID, wrapper);
+      }
+   }
+
+   public void nodeDown(String nodeID)
+   {
+   }
+
+   /**
+    * this will go through the list of retries
+    */
+   private void checkFailures()
+   {
+      final HashSet<WildFlyRecoveryDiscovery> failures = new HashSet<WildFlyRecoveryDiscovery>();
+
+      // it will transfer all the discoveries to a new collection
+      synchronized (failedDiscoverySet)
+      {
+         failures.addAll(failedDiscoverySet);
+         failedDiscoverySet.clear();
+      }
+
+      if (failures.size() > 0)
+      {
+         // This shouldn't happen on a regular scenario, however when this retry happens this needs
+         // to be done on a new thread
+         Thread t = new Thread("ActiveMQ Recovery Discovery Reinitialization")
+         {
+            @Override
+            public void run()
+            {
+               for (WildFlyRecoveryDiscovery discovery : failures)
+               {
+                  try
+                  {
+                     WildFlyActiveMQLogger.LOGGER.debug("Retrying discovery " + discovery);
+                     discovery.start(true);
+                  }
+                  catch (Throwable e)
+                  {
+                     WildFlyActiveMQLogger.LOGGER.warn(e.getMessage(), e);
+                  }
+               }
+            }
+         };
+
+         t.start();
+      }
+   }
+
+   /**
+    * @param networkConfiguration
+    * @return
+    */
+   private TransportConfiguration[] extractTransportConfiguration(Pair<TransportConfiguration, TransportConfiguration> networkConfiguration)
+   {
+      if (networkConfiguration.getB() != null)
+      {
+         return new TransportConfiguration[]{networkConfiguration.getA(), networkConfiguration.getB()};
+      }
+      return new TransportConfiguration[]{networkConfiguration.getA()};
+   }
+
+}
---
+++ src/main/java/org/apache/activemq/artemis/wildfly/security/JBossASSecurityManager.java
@@ -0,0 +1,318 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2005-2008, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.apache.activemq.artemis.wildfly.security;
+
+import java.security.AccessController;
+import java.security.Principal;
+import java.security.PrivilegedAction;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.naming.InitialContext;
+import javax.security.auth.Subject;
+
+import org.apache.activemq.artemis.core.security.CheckType;
+import org.apache.activemq.artemis.core.security.Role;
+import org.apache.activemq.artemis.core.server.ActiveMQComponent;
+import org.apache.activemq.artemis.spi.core.security.ActiveMQSecurityManager;
+import org.apache.activemq.artemis.wildfly.ActiveMQJBossLogger;
+import org.jboss.security.AuthenticationManager;
+import org.jboss.security.RealmMapping;
+import org.jboss.security.SecurityContext;
+import org.jboss.security.SecurityContextAssociation;
+import org.jboss.security.SimplePrincipal;
+
+/**
+ * This implementation delegates to the JBoss AS security interfaces (which in turn use JAAS)
+ * It can be used when running ActiveMQ in JBoss AS
+ *
+ * @author <a href="ataylor@redhat.com">Andy Taylor</a>
+ * @author <a href="tim.fox@jboss.com">Tim Fox</a>
+ */
+public class JBossASSecurityManager implements ActiveMQSecurityManager, ActiveMQComponent
+{
+   // Static --------------------------------------------------------
+
+   // Attributes ----------------------------------------------------
+
+   private final boolean trace = ActiveMQJBossLogger.LOGGER.isTraceEnabled();
+
+   /**
+    * the realmmapping
+    */
+   private RealmMapping realmMapping;
+
+   /**
+    * the JAAS Authentication Manager
+    */
+   private AuthenticationManager authenticationManager;
+
+   /**
+    * The JNDI name of the AuthenticationManager(and RealmMapping since they are the same object).
+    */
+   private String securityDomainName = "java:/jaas/activemq";
+
+   private boolean started;
+
+   private boolean isAs5 = true;
+
+   private boolean allowClientLogin = false;
+
+   private boolean authoriseOnClientLogin = false;
+
+   public boolean validateUser(final String user, final String password)
+   {
+      SimplePrincipal principal = new SimplePrincipal(user);
+
+      char[] passwordChars = null;
+
+      if (password != null)
+      {
+         passwordChars = password.toCharArray();
+      }
+
+      Subject subject = new Subject();
+
+      return authenticationManager.isValid(principal, passwordChars, subject);
+   }
+
+   public boolean validateUserAndRole(final String user,
+                                      final String password,
+                                      final Set<Role> roles,
+                                      final CheckType checkType)
+   {
+      if (allowClientLogin && SecurityContextAssociation.isClient())
+      {
+         return authoriseOnClientLogin ? useClientAuthentication(roles, checkType) : true;
+      }
+      else
+      {
+         return useConnectionAuthentication(user, password, roles, checkType);
+      }
+   }
+
+   private boolean useConnectionAuthentication(final String user,
+                                               final String password,
+                                               final Set<Role> roles,
+                                               final CheckType checkType)
+   {
+      return AccessController.doPrivileged(new PrivilegedAction<Boolean>()
+      {
+         public Boolean run()
+         {
+
+            SimplePrincipal principal = user == null ? null : new SimplePrincipal(user);
+
+            char[] passwordChars = null;
+
+            if (password != null)
+            {
+               passwordChars = password.toCharArray();
+            }
+
+            Subject subject = new Subject();
+
+            boolean authenticated = authenticationManager.isValid(principal, passwordChars, subject);
+            // Authenticate. Successful authentication will place a new SubjectContext on thread local,
+            // which will be used in the authorization process. However, we need to make sure we clean up
+            // thread local immediately after we used the information, otherwise some other people
+            // security my be screwed up, on account of thread local security stack being corrupted.
+            if (authenticated)
+            {
+               pushSecurityContext(principal, passwordChars, subject);
+               Set<Principal> rolePrincipals = getRolePrincipals(checkType, roles);
+
+               authenticated = realmMapping.doesUserHaveRole(principal, rolePrincipals);
+
+               if (trace)
+               {
+                  ActiveMQJBossLogger.LOGGER.trace("user " + user +
+                                                   (authenticated ? " is " : " is NOT ") +
+                                                   "authorized");
+               }
+               popSecurityContext();
+            }
+            return authenticated;
+         }
+      });
+   }
+
+   private boolean useClientAuthentication(final Set<Role> roles, final CheckType checkType)
+   {
+      return AccessController.doPrivileged(new PrivilegedAction<Boolean>()
+      {
+         public Boolean run()
+         {
+            SecurityContext sc = SecurityContextAssociation.getSecurityContext();
+            Principal principal = sc.getUtil().getUserPrincipal();
+
+            char[] passwordChars = (char[])sc.getUtil().getCredential();
+
+            Subject subject = sc.getSubjectInfo().getAuthenticatedSubject();
+
+            boolean authenticated = authenticationManager.isValid(principal, passwordChars, subject);
+
+            if (authenticated)
+            {
+               Set<Principal> rolePrincipals = getRolePrincipals(checkType, roles);
+
+               authenticated = realmMapping.doesUserHaveRole(principal, rolePrincipals);
+
+               if (trace)
+               {
+                  ActiveMQJBossLogger.LOGGER.trace("user " + principal.getName() +
+                                                   (authenticated ? " is " : " is NOT ") +
+                                                   "authorized");
+               }
+            }
+            return authenticated;
+         }
+
+      });
+
+   }
+
+   private void popSecurityContext()
+   {
+      if (isAs5)
+      {
+         SecurityActions.popSubjectContext();
+      }
+      else
+      {
+         AS4SecurityActions.popSubjectContext();
+      }
+   }
+
+   private void pushSecurityContext(final SimplePrincipal principal, final char[] passwordChars, final Subject subject)
+   {
+      if (isAs5)
+      {
+         SecurityActions.pushSubjectContext(principal, passwordChars, subject, securityDomainName);
+      }
+      else
+      {
+         AS4SecurityActions.pushSubjectContext(principal, passwordChars, subject);
+      }
+   }
+
+   public void addRole(final String user, final String role)
+   {
+      // NO-OP
+   }
+
+   public void addUser(final String user, final String password)
+   {
+      // NO-OP
+   }
+
+   public void removeRole(final String user, final String role)
+   {
+      // NO-OP
+   }
+
+   public void removeUser(final String user)
+   {
+      // NO-OP
+   }
+
+   public void setDefaultUser(final String username)
+   {
+      // NO-OP
+   }
+
+   private Set<Principal> getRolePrincipals(final CheckType checkType, final Set<Role> roles)
+   {
+      Set<Principal> principals = new HashSet<Principal>();
+      for (Role role : roles)
+      {
+         if (checkType.hasRole(role))
+         {
+            principals.add(new SimplePrincipal(role.getName()));
+         }
+      }
+      return principals;
+   }
+
+   public void setRealmMapping(final RealmMapping realmMapping)
+   {
+      this.realmMapping = realmMapping;
+   }
+
+   public void setAuthenticationManager(final AuthenticationManager authenticationManager)
+   {
+      this.authenticationManager = authenticationManager;
+   }
+
+   /**
+    * lifecycle method, needs to be called
+    *
+    * @throws Exception
+    */
+   public synchronized void start() throws Exception
+   {
+      if (started)
+      {
+         return;
+      }
+
+      InitialContext ic = new InitialContext();
+      authenticationManager = (AuthenticationManager)ic.lookup(securityDomainName);
+      realmMapping = (RealmMapping)authenticationManager;
+
+      started = true;
+   }
+
+   public synchronized void stop()
+   {
+      if (!started)
+      {
+         return;
+      }
+      started = false;
+   }
+
+   public synchronized boolean isStarted()
+   {
+      return started;
+   }
+
+   public void setSecurityDomainName(final String securityDomainName)
+   {
+      this.securityDomainName = securityDomainName;
+   }
+
+   public void setAs5(final boolean as5)
+   {
+      isAs5 = as5;
+   }
+
+   public void setAllowClientLogin(final boolean allowClientLogin)
+   {
+      this.allowClientLogin = allowClientLogin;
+   }
+
+   public void setAuthoriseOnClientLogin(final boolean authoriseOnClientLogin)
+   {
+      this.authoriseOnClientLogin = authoriseOnClientLogin;
+   }
+}
